9f0e706ef3dbe450f0f16e86e7e78dbf27dad37c
chiouya <chiouyazo@gmail.com>
2025-04-09 12:02:49 +0200

    Updated export script, and moved drawio files to seperate folder

--- DrawIODOC/exportMacos.sh
+++ DrawIODOC/exportMacos.sh
@@ -1,3 +1,1 @@
-/Applications/draw.io.app/Contents/MacOS/draw.io -x  OpenApi.drawio -b 10 -o Export/OpenApi.png
-/Applications/draw.io.app/Contents/MacOS/draw.io -x  TrainSetup.drawio -b 10 -o Export/TrainSetup.png
-/Applications/draw.io.app/Contents/MacOS/draw.io -x  AI_Control.drawio -b 10 -o Export/AI_Control.png
+/Applications/draw.io.app/Contents/MacOS/draw.io -x  files/ -b 10 -s 2 -f png -o Export/
